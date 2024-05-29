c68aa9c076e2543ee1544027e7c1367737926f2c
shreerajshetty <rajshetty727@gmail.com>
2024-05-29 14:42:48 +0530

    fefe

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,6 +1,5 @@
 def call(String repoUrl, String branch){
    def workingDir = "${env.WORKSPACE}"
-   bat "if exist goAwsSdkProj rmdir /s /q goAwsSdkProj"
    bat "git clone ${repoUrl} ${workingDir}"
    bat "git checkout ${branch}"
    return workingDir
